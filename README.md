1ffc71715d3427c5c38010c8993297029edcede8
Maximilian Köhl <mail@koehlma.de>
2016-06-28 01:41:25 +0200

    working proximity sensors

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tin-Puck
 Welcome to the Tin-Puck library.
+
+Inspired by the E-Puck standard library.
--- src/adc.c
+++ src/adc.c
@@ -16,40 +16,40 @@
 
 #include "tinpuck.h"
 
-#define SAMPLE_FREQ 3840.0
-
 #define MODE_ANALOG 0
 #define MODE_DIGITAL 1
 
-#define PULSE_IR0_PORT _LATF7	    // pulse IR 0 and 4
-#define PULSE_IR1_PORT _LATF8	    // pulse IR 1 and 5
-#define PULSE_IR2_PORT _LATG0	    // pulse IR 2 and 6
-#define PULSE_IR3_PORT _LATG1       // pulse IR 3 and 7
+#define PULSE_IR0_PORT _LATF7	 // pulse IR 0 and 4
+#define PULSE_IR1_PORT _LATF8	// pulse IR 1 and 5
+#define PULSE_IR2_PORT _LATG0	// pulse IR 2 and 6
+#define PULSE_IR3_PORT _LATG1   // pulse IR 3 and 7
 
 #define PULSE_IR0_DIR _TRISF7
 #define PULSE_IR1_DIR _TRISF8
 #define PULSE_IR2_DIR _TRISG0
 #define PULSE_IR3_DIR _TRISG1
 
-/* first in front right of robot than turning clockwise */
-#define IR0 8   // ir proximity sensor 0 on AD channel 8
-#define IR1 9   // ir proximity sensor 1 on AD channel 9
-#define IR2 10  // ir proximity sensor 2 on AD channel 10
-#define IR3 11  // ir proximity sensor 3 on AD channel 11
-#define IR4 12  // ir proximity sensor 4 on AD channel 12
-#define IR5 13  // ir proximity sensor 5 on AD channel 13
-#define IR6 14  // ir proximity sensor 6 on AD channel 14
-#define IR7 15  // ir proximity sensor 7 on AD channel 15
+#define BASE_SAMPLE_FREQ 16384.0
 
-#define SCAN_IR04 ((ON << IR0) | (ON << IR4))
-#define SCAN_IR15 ((ON << IR1) | (ON << IR5))
-#define SCAN_IR26 ((ON << IR2) | (ON << IR6))
-#define SCAN_IR37 ((ON << IR3) | (ON << IR7))
+#define PROXIMITY_START (int) (BASE_SAMPLE_FREQ / 256.0)
+#define PROXIMITY_PULSE (int) (0.0003 * BASE_SAMPLE_FREQ)
 
-static volatile int tin_proximity_ambient[] = {0, 0, 0, 0, 0, 0, 0, 0};
-static volatile int tin_proximity_reflection[] = {0, 0, 0, 0, 0, 0, 0, 0};
+#define ADCS_2_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 2) -1)
+#define ADCS_3_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 3) -1)
+#define ADCS_4_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 4) -1)
+#define ADCS_5_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 5) -1)
+#define ADCS_6_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 6) -1)
+#define ADCS_7_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 7) -1)
+#define ADCS_8_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 8) -1)
+#define ADCS_9_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 9) -1)
+#define ADCS_10_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 10) -1)
+#define ADCS_11_CHANNEL	(unsigned int) (2.0 * FCY / (BASE_SAMPLE_FREQ * (14 + 1) * 11) -1)
 
-static volatile int tin_proximity_calibration[] = {0, 0, 0, 0, 0, 0, 0, 0};
+static int tin_proximity_ambient[8];
+static int tin_proximity_reflected[8];
+static int tin_proximity_offset[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+static unsigned int tin_proximity_counter = 0;
 
 void tin_init_adc(void) {
     PULSE_IR0_DIR = OUTPUT;
@@ -110,8 +110,8 @@ void tin_init_adc(void) {
 
     // set automatic sample time
     ADCON3bits.SAMC = 1;
-    // set conversion clock time
-    ADCON3bits.ADCS = (unsigned int) (2.0 * FCY / (SAMPLE_FREQ * (14 + 1) * 8) - 1);
+    // set conversion clock
+    ADCON3bits.ADCS = ADCS_8_CHANNEL;
 
     // clear ADC interrupt flag
     IFS0bits.ADIF = OFF;
@@ -122,50 +122,95 @@ void tin_init_adc(void) {
     ADCON1bits.ADON = ON;
 }
 
-void __attribute__((__interrupt__, auto_psv)) _ADCInterrupt (void) {
-    IFS0bits.ADIF = OFF;
+void __attribute__((__interrupt__, auto_psv)) _ADCInterrupt(void) {
+    IFS0bits.ADIF = 0;
 
-    static unsigned int state = 0;
+    static unsigned int counter = 0;
+    static unsigned int sensors = 0;
 
-    switch (state) {
-        case 0:
-            tin_proximity_ambient[0] = ADCBUF0;
-            tin_proximity_ambient[1] = ADCBUF1;
-            tin_proximity_ambient[2] = ADCBUF2;
-            tin_proximity_ambient[3] = ADCBUF3;
-            tin_proximity_ambient[4] = ADCBUF4;
-            tin_proximity_ambient[5] = ADCBUF5;
-            tin_proximity_ambient[6] = ADCBUF6;
-            tin_proximity_ambient[7] = ADCBUF7;
-            PULSE_IR0_PORT = ON;
-            PULSE_IR1_PORT = ON;
-            PULSE_IR2_PORT = ON;
-            PULSE_IR3_PORT = ON;
-            state = 1;
-            break;
-        case 1:
-            state = 2;
-            break;
-        case 2:
-            tin_proximity_reflection[0] = ADCBUF0;
-            tin_proximity_reflection[1] = ADCBUF1;
-            tin_proximity_reflection[2] = ADCBUF2;
-            tin_proximity_reflection[3] = ADCBUF3;
-            tin_proximity_reflection[4] = ADCBUF4;
-            tin_proximity_reflection[5] = ADCBUF5;
-            tin_proximity_reflection[6] = ADCBUF6;
-            tin_proximity_reflection[7] = ADCBUF7;
-            PULSE_IR0_PORT = OFF;
-            PULSE_IR1_PORT = OFF;
-            PULSE_IR2_PORT = OFF;
-            PULSE_IR3_PORT = OFF;
-            state = 0;
-            break;
-        default:
-            state = 0;
+    if (counter == PROXIMITY_START - 1) {
+        switch (sensors) {
+            case 0:
+                tin_proximity_ambient[0] = ADCBUF0;
+                tin_proximity_ambient[4] = ADCBUF4;
+                PULSE_IR0_PORT = ON;
+                break;
+            case 1:
+                tin_proximity_ambient[1] = ADCBUF1;
+                tin_proximity_ambient[5] = ADCBUF5;
+                PULSE_IR1_PORT = ON;
+                break;
+            case 2:
+                tin_proximity_ambient[2] = ADCBUF2;
+                tin_proximity_ambient[6] = ADCBUF6;
+                PULSE_IR2_PORT = ON;
+                break;
+            case 3:
+                tin_proximity_ambient[3] = ADCBUF3;
+                tin_proximity_ambient[7] = ADCBUF7;
+                PULSE_IR3_PORT = ON;
+                break;
+            default:
+                sensors = 0;
+        }
+
+    } else if (counter == PROXIMITY_PULSE - 1) {
+        switch (sensors) {
+            case 0:
+                tin_proximity_reflected[0] = ADCBUF0;
+                tin_proximity_reflected[4] = ADCBUF4;
+                PULSE_IR0_PORT = OFF;
+                break;
+            case 1:
+                tin_proximity_reflected[1] = ADCBUF1;
+                tin_proximity_reflected[5] = ADCBUF5;
+                PULSE_IR1_PORT = OFF;
+                break;
+            case 2:
+                tin_proximity_reflected[2] = ADCBUF2;
+                tin_proximity_reflected[6] = ADCBUF6;
+                PULSE_IR2_PORT = OFF;
+                break;
+            case 3:
+                tin_proximity_reflected[3] = ADCBUF3;
+                tin_proximity_reflected[7] = ADCBUF7;
+                PULSE_IR3_PORT = OFF;
+                break;
+            default:
+                sensors = 0;
+        }
+        sensors++;
+        if (sensors >= 4) {
+            sensors = 0;
+            tin_proximity_counter++;
+        }
+    }
+    if (counter++ >= PROXIMITY_START) {
+        counter = 0;
     }
 }
 
+void tin_calibrate_proximity() {
+    unsigned int counter, previous, index;
+    unsigned int proximity[8];
+
+    for (index = 0; index < 8; index++) {
+        tin_proximity_offset[index] = 0;
+    }
+
+    for (counter = 0; counter < 10; counter++) {
+        tin_get_proximity_raw(proximity);
+        previous = tin_proximity_counter;
+        for (index = 0; index < 8; index++) {
+            tin_proximity_offset[index] += proximity[index];
+        }
+        while (previous == tin_proximity_counter);
+    }
+
+    for (index = 0; index < 8; index++) {
+        tin_proximity_offset[index] = (unsigned int) (tin_proximity_offset[index] / 10.0);
+    }
+}
 
 void tin_get_ir_ambient(unsigned int* ir_ambient) {
     unsigned int index;
@@ -177,7 +222,7 @@ void tin_get_ir_ambient(unsigned int* ir_ambient) {
 void tin_get_ir_reflection(unsigned int* ir_reflection) {
     unsigned int index;
     for (index = 0; index < 8; index++) {
-        ir_reflection[index] = (unsigned int) tin_proximity_reflection[index];
+        ir_reflection[index] = (unsigned int) tin_proximity_reflected[index];
     }
 }
 
@@ -185,26 +230,17 @@ void tin_get_proximity_raw(unsigned int* proximity) {
     int value;
     unsigned int index;
     for (index = 0; index < 8; index++) {
-        value = tin_proximity_reflection[index] - tin_proximity_ambient[index];
+        value = tin_proximity_ambient[index] - tin_proximity_reflected[index];
         proximity[index] = value > 0 ? (unsigned int) value : 0;
     }
 }
 
-void tin_calibrate_proximity() {
-    unsigned int index;
-    unsigned int proximity[8];
-    tin_get_proximity_raw(proximity);
-    for (index = 0; index < 8; index++) {
-        tin_proximity_calibration[index] = proximity[index];
-    }
-}
-
-void tin_get_proximity(unsigned int* proximity) {
+void tin_get_proximity(unsigned int *proximity) {
     int value;
     unsigned int index;
     tin_get_proximity_raw(proximity);
     for (index = 0; index < 8; index++) {
-        value = proximity[index] - tin_proximity_calibration[index];
+        value = proximity[index] - tin_proximity_offset[index];
         proximity[index] = value > 0 ? (unsigned int) value : 0;
     }
 }
